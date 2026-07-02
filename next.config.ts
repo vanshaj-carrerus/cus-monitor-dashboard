d1730ba7866a1ca81fb144ce7305676da8ea8e23
Vanshaj <vanshaj@careerussolution.com>
2026-07-02 12:23:45 +0530

    imagekit image show allowed via next config

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
-    domains: ["res.cloudinary.com"],
+    domains: ["res.cloudinary.com", "ik.imagekit.io"],
   },
 };
 
